0dab15c2d71f32a86ca856dafbff32b4db64fb3a
unknown <robertgicheha3@gmail.com>
2023-04-25 15:42:49 +0300

    worked on feedback stats and reactivity

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import Header from './components/Header'
-import FeedbackList from './components/FeedbackList'
-import FeedbackData from './data/FeedbackData'
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
 
- const [feedback , setFeedback] = useState (FeedbackData)
-
- const deleteFeedback = (id =>{
-  // console.log('App', id);
-  if(window.confirm('Are you sure you want to delete?')){
-    setFeedback(feedback.filter((item) => item.id !== id)) 
-  }
- })
-
+  const deleteFeedback = (id) => {
+    // console.log('App', id);
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
 
   return (
     <>
-    <Header/>
-    <div className='container'> 
-      <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
-    </div>
-
+      <Header />
+      <div className="container">
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types'
+
+function FeedbackStats({feedback}) {
+    //Calculate Average
+
+    let average = feedback.reduce ((acc, cur) =>{
+        return acc + cur.rating
+
+    }, 0)  / feedback.length
+
+    average = average.toFixed(1).replace(/[.,]0$/, '')
+
+    // console.log(average);
+  return (
+    <div className="feedback-stats">
+        <h4>{feedback.length}Reviews</h4>
+        <h4>Average Rating: {isNaN(average) ? 0 : average} </h4>
+    </div>
+  )
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired
+}
+
+export default FeedbackStats
