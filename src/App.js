886ea66eec387ffa6204f4983d3e96ea788e40e4
unknown <robertgicheha3@gmail.com>
2023-04-25 15:25:13 +0300

    worked on feedback stats and reactivity

--- src/App.js
+++ src/App.js
@@ -8,11 +8,19 @@ function App() {
 
  const [feedback , setFeedback] = useState (FeedbackData)
 
+ const deleteFeedback = (id =>{
+  // console.log('App', id);
+  if(window.confirm('Are you sure you want to delete?')){
+    setFeedback(feedback.filter((item) => item.id !== id)) 
+  }
+ })
+
+
   return (
     <>
     <Header/>
     <div className='container'> 
-      <FeedbackList feedback={feedback}/>
+      <FeedbackList feedback={feedback} handleDelete={deleteFeedback}/>
     </div>
 
     </>
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,19 @@
 import PropTypes from 'prop-types'
-
+import {FaTimes} from 'react-icons/fa'
 import Card from "./shared/Card";
 
-function FeedbackItem({ item }) {
+function FeedbackItem({ item , handleDelete }) {
+  // const handleClick = (id)=>{
+  //   console.log(id);
+  // }
+
+
   return (
     <Card>
       <div className="num-display">{item.rating}</div> 
+      <button onClick={() => handleDelete (item.id)} className="close">
+        <FaTimes color='purple' />
+      </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
